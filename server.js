a355b3bee97d6a0d1e364d2692b550595af687be
adamburich <adamburich@github.com>
2020-09-03 14:08:53 -0400

    Work
    Work

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var url = 'mongodb+srv://user:Password@cluster0.ybmx2.mongodb.net/<dbname>?retryWrites=true&w=majority';
 var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://user:Password@cluster0.ybmx2.mongodb.net/mini-irc?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+client.connect(err => {
+  const collection = client.db("mini-irc").collection("messages");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -13,15 +21,11 @@ var server = app.listen(7777, ()=>{
     console.log("Server started on port " + server.address().port);
 });
 
+var Message = mongoose.model("Message", {name : String, message : String});
+
 io.on('connection', () =>{
     console.log('connected');
 });
-
-mongoose.connect(url , (err) => { 
-    console.log("Connected to mongodb", err);
-});
-
-var Message = mongoose.model("Message", {name : String, message : String});
 
 app.use(express.static(__dirname));
 
