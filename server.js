7d461864a08d15005349a2cf0bb91f12c7ceeb9b
adamburich <adamburich@github.com>
2020-09-04 11:09:27 -0400

    Working

--- server.js
+++ server.js
@@ -1,46 +1,65 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://user:Password@cluster0.ybmx2.mongodb.net/mini-irc?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-
-client.connect(err => {
-  const collection = client.db("mini-irc").collection("messages");
-  // perform actions on the collection object
-  client.close();
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-var server = app.listen(7777, ()=>{
-    console.log("Server started on port " + server.address().port);
-});
-
-var Message = mongoose.model("Message", {name : String, message : String});
-
-io.on('connection', () =>{
-    console.log('connected');
-});
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/messages', (req, res) => {
-    message.find({},(err, messages)=> {
-      res.send(messages);
-    });
-});
+var Message = mongoose.model('Message',{
+  name : String,
+  message : String
+})
 
-app.post('/messages', (req, res) => {
+var database_url = 'mongodb+srv://user:Password@cluster0.ybmx2.mongodb.net/mini-irc?retryWrites=true&w=majority'
+
+app.get('/msg', (req, res) => {
+  Message.find({},(err, msg)=> {
+    res.send(msg);
+  })
+})
+
+
+app.get('/msg/:user', (req, res) => {
+  var user = req.params.user
+  Message.find({name: user},(err, msg)=> {
+    res.send(msg);
+  })
+})
+
+
+app.post('/msg', async (req, res) => {
+  try{
     var message = new Message(req.body);
-    message.save((err) =>{
-      if(err)
-        sendStatus(500);
+
+    var savedMessage = await message.save()
+      console.log('Wrote message to database');
       io.emit('message', req.body);
       res.sendStatus(200);
-    });
-  });
+  }
+  catch (error){
+    res.sendStatus(500);
+    return console.log('Error posting message',error);
+  }
+  finally{
+    console.log('Posted message')
+  }
+
+})
+
+
+
+io.on('connection', () =>{
+  console.log('User connected')
+})
+
+mongoose.connect(database_url ,{useMongoClient : true} ,(err) => {
+  console.log('Connected',err);
+})
+
+var server = http.listen(7777, () => {
+  console.log('Server running on port ', server.address().port);
+});
